7f6907eb86dfcb05996f5ce853689d8b6bb863f0
utesurdako <arktnels@gmail.com>
2021-06-22 21:59:02 +0300

    Удалены логи

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -7,13 +7,12 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start");
-        Debug.LogWarning("Warning");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.LogError("Error");
+        
     }
 }
